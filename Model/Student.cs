360e87f915ae36139e11fe023f2439a2c3600470
Kate <poznahirko.cat@yandex.ru>
2024-11-24 21:25:28 +0700

    Добавьте файлы проекта.

---
+++ Model/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Student : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public string Name { get; set; }
+        
+        public string Group { get; set; }
+        
+        public string Speciality { get; set; }
+       
+    }
+}
